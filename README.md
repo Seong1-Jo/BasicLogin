3def0aeed321ece4569225c4e60ebd0036997baf
Seong1-Jo <seong12yada@gmail.com>
2021-09-08 19:18:56 +0900

    #4 Events part Two 쉬어가는 타임- 링크로 활용해 기본동작막는 방법

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 1. Input Values
 2. Form submission
 3. Events
+4. Events part Two
--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");   //find loginform instead(document):loginForm이 html element이기때문에 document를 안쓰고 바로 loginForm을 써도 가능하다.
 
+const link = document.querySelector("a");
+
 
 function onLoginSubmit(event) { //이곳에하는 일은 브라우저가 기본 동작을 실해하지 못하게 막아주는것뿐
     event.preventDefault(); //preventDefault함수는 이벤트에서 기본으로 하는것을 막아준다.
     console.log(loginInput.value);
 }
 
+function handleLinkClick(stopp) {
+    stopp.preventDefault();
+    console.dir(stopp);
+
+    
+}
+
 loginForm.addEventListener("submit", onLoginSubmit); //submit은 엔터를 누르거나 클릭할때 발생!(click,enter기능을 둘다가짐)
+
+link.addEventListener("click", handleLinkClick);
+
+//함수를 함수(); 로 하면 함수한번만 실행하고 끝 그래서 이벤트리스너에서는 ()를 쓰지않고 실해하면, 한번만이아닌 하고싶을때 쓸수있다. addEventListener안에있는 함수는 직접실행하지않는다. 그말은 ()를 넣고 사용하지 않는다는말.
