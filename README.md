537d16aa2dd6c0bcf83ee5ca88b582cb91b8e1d7
Seong1-Jo <seong12yada@gmail.com>
2021-09-08 17:22:33 +0900

    #3 Events submit의현상-  브라우저 기본동작을 멈춘기능-- 함수에 argument을 주어서 preventDefault함수를이용

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # BasicLogin
 
 1. Input Values
+2. Form submission
+3. Events
--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");   //find loginform instead(document):loginForm이 html element이기때문에 document를 안쓰고 바로 loginForm을 써도 가능하다.
-const loginBtn = loginForm.querySelector("button");
 
-function btnClick() {
-    const username = loginInput.value;
-    console.log(username);
+
+function onLoginSubmit(event) { //이곳에하는 일은 브라우저가 기본 동작을 실해하지 못하게 막아주는것뿐
+    event.preventDefault(); //preventDefault함수는 이벤트에서 기본으로 하는것을 막아준다.
+    console.log(loginInput.value);
 }
 
-loginBtn.addEventListener("click", btnClick); //클릭했을때 이벤트리스너
-
-//위에와 같은 방법으로 const loginInput = document.querySelector("#login-form input"); ,const loginBtn = document.querySelector("#login-form button"); 사용가능
+loginForm.addEventListener("submit", onLoginSubmit); //submit은 엔터를 누르거나 클릭할때 발생!(click,enter기능을 둘다가짐)
