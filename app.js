f95686b368cb3ecb733baa29532b8e41e42ac6e2
Seong1-Jo <seong12yada@gmail.com>
2021-09-08 16:56:12 +0900

    #2 form 안에 input가고, form으로 인해 submit현상

--- app.js
+++ app.js
@@ -4,11 +4,7 @@ const loginBtn = loginForm.querySelector("button");
 
 function btnClick() {
     const username = loginInput.value;
-    if(username === "") { //만약value값이 없을때
-        alert("please write your name");
-    } else if (username.length > 15) {
-        alert("Your name is Long!!")
-    }
+    console.log(username);
 }
 
 loginBtn.addEventListener("click", btnClick); //클릭했을때 이벤트리스너
