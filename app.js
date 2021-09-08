698e0acdf39247430de901db64a4be3d84d9a34b
Seong1-Jo <seong12yada@gmail.com>
2021-09-08 19:51:48 +0900

    #5 Getting Username -step2 입력값이 <h2>Element에 표시하기(class를 추가 삭제)

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");   //find loginform instead(document):loginForm이 html element이기때문에 document를 안쓰고 바로 loginForm을 써도 가능하다.
+const greeting = document.querySelector("#greeting");
+
+const HIDE_GET = "hidden"; //대문자로 사용하는것은 string일때 중요하지않은것으로 관습?!
 
 function onLoginSubmit(event) { //이곳에하는 일은 브라우저가 기본 동작을 실해하지 못하게 막아주는것뿐
     event.preventDefault(); //preventDefault함수는 이벤트에서 기본으로 하는것을 막아준다.
     const username = loginInput.value;
-    loginForm.classList.add("hidden");
-    console.log(username);
+    loginForm.classList.add(HIDE_GET);
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDE_GET);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit); //submit은 엔터를 누르거나 클릭할때 발생!(click,enter기능을 둘다가짐)
