3efd518f3b140e3a717b23f5cb7f59e834b1a0d9
Seong1-Jo <seong12yada@gmail.com>
2021-09-14 17:42:23 +0900

    #2 Timeouts and Dates

--- clock.js
+++ clock.js
@@ -1,8 +1,11 @@
 const clock = document.querySelector("h2#clock");
 
-function sayHello() {
-    console.log("hello");
+function getClock() {
+    const date = new Date();  //new Date()는 현재 시간날짜 를 가져온다.
+    clock.innerText = `${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()}`;
 }
 
+getClock();
+setInterval(getClock, 1000);
 
-setInterval(sayHello, 5000); //자바스크립트의 지정된함수로, sayHello함수가 5초마다 실행된다는뜻 
+// setTimeout(sayHello, 5000); //Interval은 5초마다 실행이된다면, setTimeout은 5초후 함수가한번실행한다
