12d0be8661ccbb2f95d3412f1306c3fde4d6ecdb
Seong1-Jo <seong12yada@gmail.com>
2021-09-16 23:23:11 +0900

    #4 Saving To Dos JSON.stringify을 이용한 저장된값들 string으로 배열로 만들기

--- todo.js
+++ todo.js
@@ -2,6 +2,12 @@ const todoForm = document.getElementById("todo-form");
 const toDoInput = todoForm.querySelector("input");//일반적으로 document.querySelector("#todo-form input")과 같다
 const toDoList = document.getElementById("todo-list");
 
+const toDos = []; //1. 저장할 Array를 만든다
+
+function saveToDos() { //3. 함수를 만들어준다, 한가지 일만하는것은 toDos Array의 내용을 localStorage에 넣는다
+    localStorage.setItem("todos", JSON.stringify(toDos)); //5.저장된값들을 배열로 만들어주기위해 먼저JSON.stringify를해주면, 저장된값들이 String로 바뀐다.
+}
+
 function deleteToDo(event) {
     const li = event.target.parentElement; //event.target.parentElement은 버튼의 부모(li)를 li변수에 담는다.parentElement는 앞의 코드의 부모를 찾아주는거다.
     li.remove();//삭제하는 코드함수
@@ -32,7 +38,9 @@ function handleToDoSubmit(event) { //text를 넣고 enter치면 일어나는 함
     const newToDo = toDoInput.value; //리셋되기전에 todoinput값을  변수 newToDo저장해둔다.
     toDoInput.value = ""; //submit인 enter을 치면 toDoInput.value값이 ""으로 바뀐다. 하지만 변수newToDo에는 값이 남아있다.
 
+    toDos.push(newToDo); // 2.newToDo를 그리기 전에 푸시해서 배열에넣어준다,localStorage에는 배열을 담을수없다, 오직 텍스트만 담을수 있다.
     paintToDo(newToDo);  //지금함수(handleToDoSubmit)가 paintToDo함수를 사용하는 코드
+    saveToDos(); //4.위에 그려주고 함수를 호출
 
 }
 
