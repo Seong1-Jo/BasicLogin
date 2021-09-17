655561223257c2e8890d6737d00098d7512703db
Seong1-Jo <seong12yada@gmail.com>
2021-09-17 15:57:12 +0900

    #6 Loading To Dos part Two 새로고침(업데이트)를 하면 새로운값을 추가할때 기존값에 덮어지는 문제를 const을 let으로 변경하며 기존값들을 유지하며 새로운값 추가

--- todo.js
+++ todo.js
@@ -4,7 +4,7 @@ const toDoList = document.getElementById("todo-list");
 
 const TODOS_KEY = "todos"; //밑에localStorage에 이름값을 중복으로 사용하기때문에 위에서 변수를 정해준다.
 
-const toDos = []; //1. 저장할 Array를 만든다
+let toDos = []; //1. 저장할 Array를 만든다 //12.문제가 생긴다 항상 시작할때마다, 빈배열로 시작하기 때문에 const에서 let으로 바꾸어준다 : 새로고침(업데이트)가 되면 기존값의 새로운값이 덮어버리는 문제때문에 기존값과 새로운값이 같이존재하기위해, 먼저 const에서 let으로 바꿔서 업데이트가 가능하도록 만든다
 
 function saveToDos() { //3. 함수를 만들어준다, 한가지 일만하는것은 toDos Array의 내용을 localStorage에 넣는다
     localStorage.setItem(TODOS_KEY, JSON.stringify(toDos)); //5.저장된값들을 배열로 만들어주기위해 먼저JSON.stringify를해주면, 저장된값들이 String로 바뀐다.
@@ -48,15 +48,18 @@ function handleToDoSubmit(event) { //text를 넣고 enter치면 일어나는 함
 
 todoForm.addEventListener("submit", handleToDoSubmit);
 
-function sayHello(item) { //10.번주석(자바스크립트는 기본적으로 아이템이라는 인자를 줄수있다.)
-    console.log("This is the turn of", item);
-}
+// function sayHello(item) { //10.번주석(자바스크립트는 기본적으로 아이템이라는 인자를 줄수있다.)
+//     console.log("This is the turn of", item);
+// }
 
 const savedToDos =  localStorage.getItem(TODOS_KEY);//6.앞에서greeting과 같이 진행할예정
 
 
 if(savedToDos !== null) { //7. 만약 savedToDos가 localStorage에 존재하면 , savedToDos === savedToDos !== null
     const parsedToDos = JSON.parse(savedToDos); //8.localStorage에있는 string값을 살아있는 javascript object로 변하게하는거(궁금하면 console.log확인!)
-    parsedToDos.forEach((item) => {console.log("This is the turn of", item)}); //9.parsedToDos는 parse로인해 Array라서 forEach라는 것을 갖고있다. forEach는 Array의 각item(배열에담겨져있는값들)에 대해 function을 실해해준다.위의예를 보면이해도움(10번주석)
-    //같은방법으로는 parsedToDos.forEach(sayHello); === parsedToDos.forEach((item) => {console.log("This is the turn of", item)});
+    toDos = parsedToDos //13.localstorage에 값을 유지하면서 새로운값을 추가하기위해,toDos(빈배열)에 parsedToDos(저장한값)을 넣어서 전에있던 값을 복원(const였다면 넣을수 없지만, let을 이용하므로서, 값을 바꿀수있기때문에)
+    parsedToDos.forEach(paintToDo); //11.이제 함수paintToDo를 이용하여 화면에지운값을 새로고침을해도 저장된값을 화면에 남아있게해주는거
+
+    // parsedToDos.forEach((item) => {console.log("This is the turn of", item)}); //9.parsedToDos는 parse로인해 Array라서 forEach라는 것을 갖고있다. forEach는 Array의 각item(배열에담겨져있는값들)에 대해 function을 실해해준다.위의예를 보면이해도움(10번주석)
+    // //예방법으로는 parsedToDos.forEach(sayHello);(이경우는 위의 주석함수이용) === parsedToDos.forEach((item) => {console.log("This is the turn of", item)});
 }
