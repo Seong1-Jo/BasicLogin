c706d3040e5b1f2ea76f9efad715ffddb1f06665
Seong1-Jo <seong12yada@gmail.com>
2021-09-17 15:26:13 +0900

    #5 Loading To Dos part One JSON.parse를 이용한 string을 살아있는 object (배열)로 만들기

--- todo.js
+++ todo.js
@@ -2,10 +2,12 @@ const todoForm = document.getElementById("todo-form");
 const toDoInput = todoForm.querySelector("input");//일반적으로 document.querySelector("#todo-form input")과 같다
 const toDoList = document.getElementById("todo-list");
 
+const TODOS_KEY = "todos"; //밑에localStorage에 이름값을 중복으로 사용하기때문에 위에서 변수를 정해준다.
+
 const toDos = []; //1. 저장할 Array를 만든다
 
 function saveToDos() { //3. 함수를 만들어준다, 한가지 일만하는것은 toDos Array의 내용을 localStorage에 넣는다
-    localStorage.setItem("todos", JSON.stringify(toDos)); //5.저장된값들을 배열로 만들어주기위해 먼저JSON.stringify를해주면, 저장된값들이 String로 바뀐다.
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos)); //5.저장된값들을 배열로 만들어주기위해 먼저JSON.stringify를해주면, 저장된값들이 String로 바뀐다.
 }
 
 function deleteToDo(event) {
@@ -45,3 +47,16 @@ function handleToDoSubmit(event) { //text를 넣고 enter치면 일어나는 함
 }
 
 todoForm.addEventListener("submit", handleToDoSubmit);
+
+function sayHello(item) { //10.번주석(자바스크립트는 기본적으로 아이템이라는 인자를 줄수있다.)
+    console.log("This is the turn of", item);
+}
+
+const savedToDos =  localStorage.getItem(TODOS_KEY);//6.앞에서greeting과 같이 진행할예정
+
+
+if(savedToDos !== null) { //7. 만약 savedToDos가 localStorage에 존재하면 , savedToDos === savedToDos !== null
+    const parsedToDos = JSON.parse(savedToDos); //8.localStorage에있는 string값을 살아있는 javascript object로 변하게하는거(궁금하면 console.log확인!)
+    parsedToDos.forEach((item) => {console.log("This is the turn of", item)}); //9.parsedToDos는 parse로인해 Array라서 forEach라는 것을 갖고있다. forEach는 Array의 각item(배열에담겨져있는값들)에 대해 function을 실해해준다.위의예를 보면이해도움(10번주석)
+    //같은방법으로는 parsedToDos.forEach(sayHello); === parsedToDos.forEach((item) => {console.log("This is the turn of", item)});
+}
